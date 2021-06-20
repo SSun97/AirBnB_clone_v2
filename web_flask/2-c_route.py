b51d00a10c4b09597e5ecfff63c3d50a26f0275a
ssun97 <sunmingyu.97@gmail.com>
2021-06-20 02:00:40 +0000

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""first flask hello world"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """basic index page """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """extra page /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """create page /c/text """
+#    return "C %s" % text.replace("_", " ")
+    return "C {}".format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
