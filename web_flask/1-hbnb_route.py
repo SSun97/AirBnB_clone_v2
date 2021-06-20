d5452783f5e4d1eb89fb9d6914e45540d2399f92
ssun97 <sunmingyu.97@gmail.com>
2021-06-20 01:19:47 +0000

    first push

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""the first flask page"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """most basic index page """
+    return "Hello HBNB!"
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ add extra page /hbnb"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
